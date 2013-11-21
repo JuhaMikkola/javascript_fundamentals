3c5f5ffb304054365db7870c1fbbf522cdce2fae
Juha Mikkola <sales@floorballpro.com>
2013-11-21 16:43:23 -0500

    JS Array Exercises done

--- example.js
+++ example.js
@@ -1,3 +1,22 @@
+
+// Exercises: Arrays
+
+// EXERCISE: Your top choices
+
+// Create an array to hold your top choices (colors, presidents, whatever).
+// For each choice, log to the screen a string like: "My #1 choice is blue."
+// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+
+// var rappers = new Array("Kanye", "Pac", "Big");
+
+// console.log("My favorite rapper is " + rappers[2])
+
+// console.log("Not a big fan of " + rappers[1])
+
+// console.log(rappers[0] + " was cool until he sold out")
+
+
+
 // Exercises: if/else statements
 
 // EXERCISE: What number's bigger?
