9ce35c2358916424b7c64964414fe86e2d96f8c3
Juha Mikkola <sales@floorballpro.com>
2013-11-22 17:06:12 -0500

    JS Objects complete

--- example.js
+++ example.js
@@ -1,3 +1,116 @@
+// document.getElementById('hplogo')
+
+// var logo = document.getElementById('hplogo')
+
+// logo
+
+// logo.src
+
+// logo.src = "http://www.digitaltrends.com/wp-content/uploads/2013/02/yahoo-voices-hacked-450-000-passwords-posted-online-7169a7e88d.jpg"
+
+
+// Exercises: Objects
+
+// var theRecipeCard = {
+ 
+//       title: "Mom's Favorite Goulash Soup",
+//       servings: 12,
+//       ingredients: ["Stew", "Meat", "Beets"]
+
+// };
+
+// console.log(theRecipeCard.title);
+// console.log("Serves: " + theRecipeCard.servings);
+// console.log("Ingredients: " + theRecipeCard.ingredients[0] + theRecipeCard.ingredients[1] + theRecipeCard.ingredients[2]);
+
+// EXERCISE: The Recipe Card
+
+// Never forget another recipe!
+
+// Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+// On separate lines (one console.log statement for each), log the recipe information so it looks like:
+// Mole
+// Serves: 2
+// Ingredients:
+// cinnamon
+// cumin
+// cocoa
+
+// EXERCISE: The Reading List
+
+// Keep track of which books you read and which books you want to read!
+
+// Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+// Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+// Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+// var favBooks = [
+
+//         {
+//         title: "You're Fuckin Out, I'm Fuckin' In",
+//         author: "Kenny Powers",    
+//         alreadyRead: false},
+
+//         {
+//         title: "Playing with Fire",
+//         author: "Theo Fleury",
+//         alreadyRead: true},
+
+//         {
+//         title: "Leader without a Title",
+//         author: "Robin Sharma",
+//         alreadyRead: true}
+
+// ];
+
+// for (var f = 0; f < favBooks.length; f++) {
+
+//   var favBook = favBooks[f];
+
+//   if (favBook.alreadyRead == true) {
+
+//     console.log( "You already read " + favBook.title + " by " + favBook.author )
+//   }
+//   else
+//   {
+//     console.log( "You need to read " + favBook.title + " by " + favBook.author )};
+
+// }
+
+// You need to read You're Fuckin Out, I'm Fuckin' In by Kenny Powers example.js:76
+// You already read Playing with Fire by Theo Fleury example.js:72
+// You already read Leader without a Title by Robin Sharma example.js:72
+
+// EXERCISE: The Movie Database
+
+// It's like IMDB, but much much smaller!
+
+// Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
+// Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
+
+var favMovies = [
+
+      { title: "Blood In Blood Out",
+        duration: 180,
+        stars: ["Damian Chapa", "Jesse Borrego", "Benjamin Bratt"]
+      },
+
+      { title: "CB4",
+        duration: 89,
+        stars: ["Chris Rock", "Allen Payne", "Deezer D"]
+      }
+
+];
+
+function printIt(movie) {
+
+    for (var r = 0; r < movie.length; r++) {
+
+      document.write(movie[r].title + " lasts for " + movie[r].duration + " minutes. Stars: " + movie[r].stars);
+    }
+}
+
+printIt(favMovies);
 
 // Exercises: Arrays
 
